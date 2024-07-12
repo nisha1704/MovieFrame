9880851c62c1285dadcfd67f688a9f17f998c5cc
nisha1704 <nisha68925@gamil.com>
2024-07-13 00:27:20 +0530

    first commit

---
+++ src/main/java/com/movieframe/movieframe/MovieframeApplication.java
@@ -0,0 +1,20 @@
+package com.movieframe.movieframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MovieframeApplication {
+	@GetMapping("/")	
+	public String hello(){
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieframe/movieframe/MovieframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieframe.movieframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
